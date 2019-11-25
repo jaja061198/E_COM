b644406d468a3fbe9d842b76bf23044879a31d02
Ronnie Lupig <jaja061198@gmail.com>
2019-11-25 10:31:20 +0800

    initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="footer-content container">
+        <div class="made-with">Feel free to contact us @</div>
+        {{-- {{ menu('footer', 'partials.menus.footer') }} --}}
+    </div> <!-- end footer-content -->
+</footer>
