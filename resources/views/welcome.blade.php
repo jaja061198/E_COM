3b4fcbb1f95ffb90e7ea2ea8380416fd81b2aeaa
ronnie lupig <jaja061198@gmail.com>
2020-01-18 19:30:59 +0800

    Font removal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,6 @@ use App\Helper\Helper;
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Montserrat%7CRoboto:300,400,700" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
